04fc1ad246257a264e0c38b17a700a267c37c526
geekfreak <geekfreak@gmail.com>
2014-07-03 02:56:01 +0200

    initial commit

---
+++ workspace.sh
@@ -0,0 +1,12 @@
+cd workspace/
+npm install -g grunt-cli
+
+git clone git@github.com:geekfreak/git-web-hook-handler.git
+cd git-web-hook-handler/
+npm install
+cd ..
+
+git clone git@github.com:uxisready/uxisready.com.git
+cd uxisready.com
+npm install
+cd ..
